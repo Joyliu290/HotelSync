af64742a4588ad7338b69f2dbf6f51fd6c2b22ab
Joyliu290 <joyliu290@hotmail.com>
2018-06-11 22:42:59 -0700

    Initial commit

---
+++ app/src/main/java/project/hotelsync/com/hotelsync/MainActivity.java
@@ -0,0 +1,27 @@
+package project.hotelsync.com.hotelsync;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView signUpNow = (TextView)findViewById(R.id.signUpLinkText);
+        final Intent signUpPage = new Intent(this, UserSignUp.class);
+        signUpNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(signUpPage);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/project/hotelsync/com/hotelsync/UserSignUp.java
@@ -0,0 +1,16 @@
+package project.hotelsync.com.hotelsync;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Joyli on 2018-06-03.
+ */
+
+public class UserSignUp extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+    }
+}
